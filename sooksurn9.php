da4b899847469cc03985758b94168201b062f9d1
Poohktt <poohktt1@gmail.com>
2022-05-07 18:59:24 +0700

    Add files via upload

---
+++ sooksurn9.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+<title>สุขสรรค์ ลั่นล้า ร่าเริง ที่สุด2</title>
+ <meta content="poohktt@homestudio" name="author">
+ <link type="text/css" rel="stylesheet" href="nnnewway10.css">
+ <link type="text/css" rel="stylesheet" href="layout.css">
+<link type="text/css" rel="stylesheet" href="component.css">
+ <link href="style.css" rel="stylesheet" type="text/css"/>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+ <style type="text/css"> 
+    div > img {
+    position:relative;
+    top:5%;
+    max-height:25%;
+    width: auto;
+    
+   display:block;
+    left:10px
+    } 
+     </style> 
+     <?php
+        include 'session.php';
+  ?>
+</head>
+
+ <body>
+     
+    <!--  <div class="container"> -->
+  <div class="topping">อันนี้ควรอยู่ด้านบน
+        <?php
+        echo "รหัสสมาชิก: " . $_SESSION['login_id'];
+        echo "<br>";
+        echo "ยินดีต้อนรับคุณ $s_login_username อีเมล์ $s_login_email";
+        ?>
+  </div>
+  <!-- Start navigator -->
+<div class="nav">
+<ul class="nav-ul">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Product</a></li>
+  <li><a href="#">Service</a></li>
+  <!-- <li><a href="#">About us</a></li> -->
+  <li><a href="logout.php">ออกจากระบบ</a></li>
+</ul>
+</div>
+<!-- End navigator -->
+<div class="leftRail">rail ที่ด้านซ้าย</div>
+<div class="center1">
+<img src="DSC01971.png"   class="resping" alt="ขนมชั้น">
+<br><img src="cracker2.png"    class="resping" alt="ขนมปังแครกเก้อ">
+</div>
+<span class="rightRail"> ที่ด้านขวา</span>
+<!--  </div>  -->
+ </body>
+</html>
